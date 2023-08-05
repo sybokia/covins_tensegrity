b72d65148bb9005bbaa7e5d9ec93e2e77b28e0e8
sw <sibo.w9@gmail.com>
2023-08-05 00:16:26 -0400

    improved docker orb frontend

---
+++ docker/euroc_play_all.sh
@@ -0,0 +1,6 @@
+source ~/Documents/scripts/multi_lab.bash
+x-terminal-emulator -e /home/sw/covins_ws/src/covins/docker/euroc_play.sh 1
+x-terminal-emulator -e /home/sw/covins_ws/src/covins/docker/euroc_play.sh 2
+x-terminal-emulator -e /home/sw/covins_ws/src/covins/docker/euroc_play.sh 3
+x-terminal-emulator -e /home/sw/covins_ws/src/covins/docker/euroc_play.sh 4
+x-terminal-emulator -e /home/sw/covins_ws/src/covins/docker/euroc_play.sh 5
